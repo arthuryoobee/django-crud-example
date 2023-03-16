21a255f7426fca953a4c67d5c13a8a879824d3c5
Arthur Lewis Yoobee <arthur.lewis@yoobeesouthseas.ac.nz>
2023-03-16 14:39:39 +1300

    Creating superuser edit

--- myproject/management/commands/createsu.py
+++ myproject/management/commands/createsu.py
@@ -9,5 +9,6 @@ class Command(BaseCommand):
 		# If it does not exist, then create a super-user with that username,
 		# Supply an email address and a password to it
 		# So, now we have an admin user to manage our AWS RDS database
+		print("WHAT HAPPENS - DOES IT EVEN GET HERE???")
 		if not User.objects.filter(username="awsadmin").exists():
 			User.objects.create_superuser("awsadmin", "arthur.lewis420@gmail.com", "AWSAdmin123")
