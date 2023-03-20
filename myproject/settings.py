54462f39df9164cd262ed582a4a4b3accbccbdaa
Arthur Lewis Yoobee <arthur.lewis@yoobeesouthseas.ac.nz>
2023-03-20 14:50:31 +1300

    Comment out storages

--- myproject/settings.py
+++ myproject/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'storages', #for static files
+    #'storages', #for static files
 
     'myproject',
     'tasks',
